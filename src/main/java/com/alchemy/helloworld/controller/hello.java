3c9892ec0a9cb48aec446fa855a4be265f73b9f6
hanpeilin <alchemylinux@gmail.com>
2022-11-15 01:20:18 +0800

    new

--- src/main/java/com/alchemy/helloworld/controller/hello.java
+++ src/main/java/com/alchemy/helloworld/controller/hello.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class hello {
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello world!";
+        return "Hello Spring!";
     }
 
 }
